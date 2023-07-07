4b290684ad2ee05c61a5b2f9a56afff8e3198dff
John Bayly <3430117+oobayly@users.noreply.github.com>
2023-07-07 13:08:49 +0100

    Add Net 6.0 test program

---
+++ Test-net6/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Configuration;
+using TipsTrade.KashFlow;
+using TipsTrade.KashFlow.KashFlowAPI;
+
+namespace Test_net6 {
+  internal class Program {
+    static async Task Main(string[] args) {
+      var config = new ConfigurationBuilder()
+        .AddUserSecrets<Program>()
+        .Build()
+        ;
+
+      var api = new KashFlowClient(config["UserName"], config["Password"]);
+
+      //var firstN = await api.GetReceiptsAsync(ReceiptFilterType.All)
+      //  .Where(x => x.Lines.Count() > 1)
+      //  .Take(250)
+      //  .ToArrayAsync();
+
+      //var receipts = await api.FindReceiptsAsync(x => {
+      //  return x.Paid == 0;
+      //});
+
+      //var tasks = new[] {
+      //  api.GetReceiptsAsync(ReceiptFilterType.Unpaid).ToArrayAsync().AsTask(),
+      //  api.GetReceiptsAsync(ReceiptFilterType.Overdue).ToArrayAsync().AsTask(),
+      //  //api.GetReceiptsAsync(ReceiptFilterType.Overpaid).ToArrayAsync().AsTask()
+      //};
+
+      //var items = (await Task.WhenAll(tasks))
+      //  .SelectMany(x=> x)
+      //  .DistinctBy(x=> x.InvoiceDBID)
+      //  ;
+
+      //var recipts = await api.GetReceiptsAsync(ReceiptFilterType.Unpaid)
+      //  .ToArrayAsync()
+      //  ;
+
+      //var payments = await api.GetInvoicePaymentsAsync(new DateTime(2023, 1, 1))
+      //  .Take(250)
+      //  .ToArrayAsync()
+      //  ;
+
+      var invoices = await api.GetInvoices_UnpaidAsync(new GetInvoices_UnpaidRequest());
+
+      Console.WriteLine("Hello, World!");
+    }
+  }
+}
